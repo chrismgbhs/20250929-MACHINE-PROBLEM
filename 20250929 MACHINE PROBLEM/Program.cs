dc992765e28e06f76318a3cbda2bafb9cab22397
Chris Boen Magbuhos <chrisboen8@outlook.com>
2025-10-01 00:33:34 +0800

    updated

--- 20250929 MACHINE PROBLEM/Program.cs
+++ 20250929 MACHINE PROBLEM/Program.cs
@@ -13,11 +13,14 @@ namespace _20250929_MACHINE_PROBLEM
         {
             char[] characters = AssignChars();
             char[] key = AssignPassPhrase(GetPassPhrase(), characters);
-            RunProgram(DetermineMode(), key);
-            Console.WriteLine();
+            RunProgram(DetermineMode(), key, characters);
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// This method determines whether the user wants to encode or decode a message.
+        /// </summary>
+        /// <returns></returns>
         static string DetermineMode()
         {
             Console.Write("Enter mode (E)ncode or (D)ecode: ");
@@ -31,9 +34,9 @@ namespace _20250929_MACHINE_PROBLEM
                     if (input == "E")
                     {
                         Console.WriteLine("You chose to Encode.");
-                        return "Enode";
+                        return "Encode";
                     }
-                    
+
                     else
                     {
                         Console.WriteLine("You chose to Decode.");
@@ -44,34 +47,44 @@ namespace _20250929_MACHINE_PROBLEM
                 else
                 {
                     Console.Write("Invalid input. Please enter (E)ncode or (D)ecode: ");
-                }      
+                }
             }
         }
 
+        /// <summary>
+        /// This method gets the pass phrase from the user.
+        /// </summary>
+        /// <returns></returns>
         static string GetPassPhrase()
         {
             Console.Write("Enter your pass phrase: ");
             string passPhrase = Console.ReadLine().ToUpper();
 
-            Console.WriteLine();
-
             return passPhrase;
         }
 
+        /// <summary>
+        /// This method assigns the characters A-Z to an array.
+        /// </summary>
+        /// <returns></returns>
         static char[] AssignChars()
         {
             char[] arr = new char[26];
             for (int counter = 65; counter <= 90; counter++)
             {
-                arr[counter-65] = (char)counter;
-                Console.Write($"    {arr[counter - 65]}");
+                arr[counter - 65] = (char)counter;
+                //Console.Write($"    {arr[counter - 65]}");
             }
-
-            Console.WriteLine();
 
             return arr;
         }
 
+        /// <summary>
+        /// This method assigns the pass phrase to the key array.
+        /// </summary>
+        /// <param name="passPhrase"></param>
+        /// <param name="characters"></param>
+        /// <returns></returns>
         static char[] AssignPassPhrase(string passPhrase, char[] characters)
         {
             int keyCounter = 0;
@@ -83,55 +96,114 @@ namespace _20250929_MACHINE_PROBLEM
                 {
                     //Console.WriteLine($"Assigning {passPhrase[passPhraseCounter]} to key[{keyCounter}]");
                     key[keyCounter] = passPhrase[passPhraseCounter];
-                    Console.Write($"    {key[keyCounter]}");
+                    //Console.Write($"    {key[keyCounter]}");
                     keyCounter++;
                 }
 
-                else
-                    Console.WriteLine("No assignment.");
+                //else
+                //Console.WriteLine("No assignment.");
             }
 
-            Console.WriteLine();
+            foreach (char character in characters)
+            {
+                if (!key.Contains(character))
+                {
+                    key[keyCounter] = character;
+                    keyCounter++;
+                }
+            }
 
             return key;
         }
 
-        static void RunProgram(string mode, char[] key)
+        /// <summary>
+        /// This method runs the program based on the mode chosen by the user.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <param name="key"></param>
+        /// <param name="characters"></param>
+        static void RunProgram(string mode, char[] key, char[] characters)
         {
             if (mode == "Encode")
-            Encode(key);
+                Encode(key, characters);
 
-            else 
-            Decode(key);
+            else
+                Decode(key, characters);
 
         }
 
-        static void Encode(char[] key)
+        /// <summary>
+        /// This method encodes the message using the key and characters arrays.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="characters"></param>
+        static void Encode(char[] key, char[] characters)
         {
             string message = GetMessage();
-            char[] characters = AssignChars();
+            OutputKey(key, characters, message);
+
+            //CheckAllArrays(characters, key);
+        }
+
+        static void OutputKey(char[] key, char[] characters, string message)
+        {
             Console.Write("KEY: ");
             foreach (char letter in message)
             {
                 if (characters.Contains(letter))
                 {
-                  for (int counter = 0, counter < characters.Length, counter++)
-                  {
-                     if (characters[counter] == letter)
-                     {
-                        Console.Write(key[counter]);
-                        break;
-                     }
-                  }
+                    for (int counter = 0; counter < characters.Length; counter++)
+                    {
+                        if (characters[counter] == letter)
+                        {
+                            Console.Write(key[counter]);
+                            break;
+                        }
+                    }
                 }
+                else
+                    Console.Write(letter);
             }
         }
 
-        static void Decode(char[] key)
+        /// <summary>
+        /// This method decodes the message using the key and characters arrays.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="characters"></param>
+        static void Decode(char[] key, char[] characters)
         {
             string keyCode = GetKey();
+            Console.Write("MESSAGE: ");
+            OutputMessage(key, characters, keyCode);
+
+            //CheckAllArrays(characters, key);
         }
 
+        static void OutputMessage(char[] key, char[] characters, string keyCode)
+        {
+            foreach (char letter in keyCode)
+            {
+                if (key.Contains(letter))
+                {
+                    for (int counter = 0; counter < key.Length; counter++)
+                    {
+                        if (key[counter] == letter)
+                        {
+                            Console.Write(characters[counter]);
+                            break;
+                        }
+                    }
+                }
+                else
+                    Console.Write(letter);
+            }
+        }
+
+        /// <summary>
+        /// This method gets the message from the user.
+        /// </summary>
+        /// <returns></returns>
         static string GetMessage()
         {
             string message;
@@ -140,14 +212,54 @@ namespace _20250929_MACHINE_PROBLEM
             return message.ToUpper();
         }
 
+        /// <summary>
+        /// This method gets the key code from the user.
+        /// </summary>
+        /// <returns></returns>
         static string GetKey()
         {
             string keyCode;
-            Console.Write("Enter your key code: ");
+            Console.Write("KEY: ");
+
             keyCode = Console.ReadLine();
-            return message.ToUpper();
+            return keyCode.ToUpper();
         }
 
-        
+        /// <summary>
+        /// This method sets the sequence of checking the arrays and groups it for easy calling.
+        /// </summary>
+        /// <param name="characters"></param>
+        /// <param name="key"></param>
+        static void CheckAllArrays(char[] characters, char[] key)
+        {
+            CheckCharacters(characters);
+            CheckKey(key);
+        }
+
+        /// <summary>
+        /// This method is just used for debugging purposes to check the characters array.
+        /// </summary>
+        /// <param name="characters"></param>
+        static void CheckCharacters(char[] characters)
+        {
+            Console.WriteLine();
+            foreach (char letter in characters)
+            {
+                Console.Write($"    {letter}");
+            }
+        }
+
+        /// <summary>
+        /// This method is just used for debugging purposes to check the key array.
+        /// </summary>
+        /// <param name="key"></param>
+        static void CheckKey(char[] key)
+        {
+            Console.WriteLine();
+            foreach (char letter in key)
+            {
+                Console.Write($"    {letter}");
+            }
+        }
     }
 }
